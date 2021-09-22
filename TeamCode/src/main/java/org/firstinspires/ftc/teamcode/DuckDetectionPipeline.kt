f9e41ac7387743ac7e36098b5ee4ceb474352fc7
bryce <brycehanna@live.com>
2021-09-22 07:48:58 -0400

    Updated DDP to actually register the zone in which the duck is in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DuckDetectionPipeline.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DuckDetectionPipeline.kt
@@ -12,12 +12,16 @@ class DuckDetectionPipeline : OpenCvPipeline() {
     // Mats defined in order of how they are used in processFrame()
     var hsv = Mat() // inputMat converted from RGB to HSV
     var mask = Mat() // hsv with isolated parts of interest color
-    val kernel =  Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(3.0, 3.0))
+    val kernel: Mat =  Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(3.0, 3.0))
     var cleanMask = Mat() // same as mask, but eroded and dilated to clean up stray pixels
     var result = Mat() // The original image with boxes around color areas
     var hierarchy = Mat()
+    var largestAreaMat = MatOfPoint()
 
     val contours = ArrayList<MatOfPoint>()
+
+    var duckZone = -1
+        private set
 
     override fun processFrame(input: Mat?): Mat {
         contours.clear()
@@ -28,15 +32,43 @@ class DuckDetectionPipeline : OpenCvPipeline() {
         Imgproc.findContours(cleanMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
 
         result.setTo(input)
-        for (c in contours) {
-            if (Imgproc.contourArea(c) < 50) continue
 
-            val boundingRect = Imgproc.boundingRect(c)
-            val pt1 = Point(boundingRect.x.toDouble(), boundingRect.y.toDouble())
-            val pt2 = Point((boundingRect.x + boundingRect.width).toDouble(),  (boundingRect.y + boundingRect.height).toDouble())
-            Imgproc.rectangle(result, pt1, pt2, Scalar(0.0, 0.0, 255.0))
+        var maxArea = 0.0
+
+        for (c in contours) {
+            val area = Imgproc.contourArea(c)
+            if (area > maxArea) {
+                maxArea = area
+                largestAreaMat = c
+
+                val boundingRect = Imgproc.boundingRect(c)
+                val pt1 = Point(boundingRect.x.toDouble(), boundingRect.y.toDouble())
+                val pt2 = Point(
+                    (boundingRect.x + boundingRect.width).toDouble(),
+                    (boundingRect.y + boundingRect.height).toDouble()
+                )
+                // Draw white box around contour bounds
+                Imgproc.rectangle(result, pt1, pt2, Scalar(0.0, 0.0, 255.0))
+            }
         }
+
+        updateZone(largestAreaMat)
+
         Imgproc.cvtColor(result, result, Imgproc.COLOR_HSV2RGB)
         return result
     }
+
+    private fun updateZone(contour: MatOfPoint) {
+        val boundingRect = Imgproc.boundingRect(contour)
+        var middle = (boundingRect.x + boundingRect.width) / 2
+        // Check if the middle is within certain ranges
+        // TODO: fix the hardcoded values, and make an enum to store ints
+        duckZone = when (middle) {
+            in 0..106 -> 0
+            in 106..213 -> 1
+            in 213..320 -> 2
+            else -> -1
+        }
+    }
+
 }
