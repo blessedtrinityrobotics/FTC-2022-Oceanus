3103f6095d760863f64e46b76c78f35a9d4ed51f
Bryce Hanna <39998963+B-Ricey763@users.noreply.github.com>
2021-10-05 17:01:52 -0400

    Created config constants for instant changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConfigConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+/**
+ * Used by FTC dashboard to change values on the fly
+ */
+@Config
+class ConfigConstants {
+    public static double motorKP = 1.0;
+    public static double motorKI = 1.0;
+    public static double motorKD = 1.0;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode
 
+import com.acmerobotics.dashboard.config.Config
+
 const val CAM_RES_X = 320
 const val CAM_RES_Y = 240
 const val CAM_NAME = "Webcam 1"
