cd6041eda0ff773a100b5fa273da6104fd2eb823
Robert007-23 <rbond23@btcatholic.org>
2021-10-12 15:30:24 -0400

    Update BasicTeleOp.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.kt
@@ -3,17 +3,20 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
-import org.firstinspires.ftc.robotcore.external.Telemetry
 
 @TeleOp(name="Basic Kotlin TeleOp") // Compatible with Java annotations
 class BasicTeleOp : OpMode() {
     // the lateinit modifier allows you to wait to initialize a variable in a neat fashion
-    private lateinit var leftDrive: DcMotor
-    private lateinit var rightDrive: DcMotor
+    private lateinit var frontLeftDrive: DcMotor
+    private lateinit var frontRightDrive: DcMotor
+    private lateinit var backLeftDrive: DcMotor
+    private lateinit var backRightDrive: DcMotor
 
     override fun init() {
-        leftDrive = hardwareMap.dcMotor.get("left_drive")
-        rightDrive = hardwareMap.dcMotor.get("right_drive")
+        frontLeftDrive = hardwareMap.dcMotor.get("front_left_drive")
+        frontRightDrive = hardwareMap.dcMotor.get("front_right_drive")
+        backLeftDrive = hardwareMap.dcMotor.get("back_left_drive")
+        backRightDrive = hardwareMap.dcMotor.get("back_right_drive")
     }
 
     override fun loop() {
@@ -21,8 +24,10 @@ class BasicTeleOp : OpMode() {
         val rightPower = gamepad1.right_stick_y
 
         // Instead of using getters/setters, just let kotlin do it for you!
-        leftDrive.power = leftPower.toDouble()
-        rightDrive.power = rightPower.toDouble()
+        frontLeftDrive.power = leftPower.toDouble()
+        frontRightDrive.power = -rightPower.toDouble()
+        backLeftDrive.power = -leftPower.toDouble()
+        backRightDrive.power = rightPower.toDouble()
 
         // This part is basically synonymous with java
         telemetry.addData("Left Power", leftPower)
